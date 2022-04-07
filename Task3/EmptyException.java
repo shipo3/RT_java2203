472ac0292aa1f18957e61975b87954e1b707ada0
shipo3 <100467635+shipo3@users.noreply.github.com>
2022-04-08 00:47:09 +0900

    例外クラス名の変更

--- Task3/EmptyException.java
+++ Task3/EmptyException.java
@@ -1,12 +1,12 @@
 package task3;
 
 //オリジナル例外定義　
-public class BrankCheckException extends Exception {
+public class EmptyException extends Exception {
     // warningを回避するための宣言
     private static final long serialVersionUID = 1L;
 
     // コンストラクタ
-    BrankCheckException(String msg) {
+    EmptyException(String msg) {
 	super(msg);
     }
 }
